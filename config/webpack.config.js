b7de3872b63fd8bb1e26e0c97d4ced192d4215a9
ant478 <ant478@gmail.com>
2019-03-01 21:39:28 +0300

    ITR-4 Added React and Babel, configured Webpack and linter

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,4 +6,28 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../frontend/build'),
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    }, {
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/react'],
+        },
+      },
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
 };
