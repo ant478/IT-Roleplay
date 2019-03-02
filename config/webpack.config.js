b579124237a1c50d8eacdddf1b18921e44e025c1
ant478 <ant478@gmail.com>
2019-03-03 02:01:04 +0300

    ITR-4 Fixed grunt tasks, added css to bundle

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './frontend/app.js',
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../frontend/build'),
@@ -28,6 +31,13 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/react'],
         },
       },
+    }, {
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader',
+      ],
     }],
   },
   resolve: {
@@ -37,12 +47,14 @@ module.exports = {
     ignored: /node_modules/,
   },
   plugins: [
-    new WebpackShellPlugin({
-      onBuildExit: ['grunt eslint:frontend'],
+    new CleanWebpackPlugin(['build'], {
+      root: path.resolve(__dirname, '../frontend'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
     }),
     new CopyPlugin([
       { from: 'frontend/index.html', to: 'index.html' },
     ]),
   ],
-  stats: 'errors-only',
 };
